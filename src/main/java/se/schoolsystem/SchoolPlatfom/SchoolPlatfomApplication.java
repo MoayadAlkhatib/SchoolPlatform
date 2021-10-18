6a0dbf77b90b07269db5c9fbe95871955a654637
moayada <moayada@kth.se>
2021-10-18 23:04:42 +0200

    Initilized the spring boot application

---
+++ src/main/java/se/schoolsystem/SchoolPlatfom/SchoolPlatfomApplication.java
@@ -0,0 +1,13 @@
+package se.schoolsystem.SchoolPlatfom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPlatfomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPlatfomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/schoolsystem/SchoolPlatfom/SchoolPlatfomApplicationTests.java
@@ -0,0 +1,13 @@
+package se.schoolsystem.SchoolPlatfom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPlatfomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
